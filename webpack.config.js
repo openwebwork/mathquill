4090da669b4fdea4b1c673d5f60a4540dc9a0976
Glenn Rice <grice1@missouriwestern.edu>
2021-11-16 05:58:10 -0600

    Add the dist files to the git repository and stop building mathquill when `npm install` is run.  Older versions of npm do not run the npm prepare script.
    The lockfile is switch back to lockfile version 2 as that won't be a
    problem anymore.

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = (env, argv) => {
 			mode: argv.mode,
 			entry,
 			output: {
-				path: path.resolve(__dirname, 'dist'),
+				path: path.resolve(__dirname, 'build'),
 				filename: '[name].js'
 			},
 			resolve: {
